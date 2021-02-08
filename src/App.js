97492a73fd6fb8601f1fa57d930fa5e072382f9d
matteoaricci <matteo.a.ricci@gmail.com>
2021-02-08 11:47:53 -0500

    modal and fetch working

--- src/App.js
+++ src/App.js
@@ -2,34 +2,35 @@ import React, { useState, useEffect } from 'react';
 import { ChakraProvider, theme } from '@chakra-ui/react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Home from './components/Home';
-import ProjectPage from './components/ProjectPage'
-import Navbar from './components/Navbar'
 
 function App() {
   const [loggedIn, setloggedIn] = useState(false);
   const [currentUser, setcurrentUser] = useState({});
-  const [projects, setProjects] = useState([])
+  const [projects, setProjects] = useState([]);
 
   useEffect(() => {
     fetch('http://localhost:3000/users/2')
       .then(response => response.json())
       .then(data => {
-        console.log(data)
-        // setcurrentUser(data.user)
-        // setProjects(data.projects)
-        setloggedIn(true)
+        const res = data[0];
+        setcurrentUser(res);
+        setProjects(res.projects);
+        setloggedIn(true);
       });
   }, []);
   return (
     <ChakraProvider theme={theme}>
-      <Navbar />
       <Switch>
         <Route path="/">
-          <Home currentUser={currentUser} projects={projects}/>
+          <Home
+            currentUser={currentUser}
+            projects={projects}
+            setProjects={setProjects}
+          />
         </Route>
-        <Route path="/project/:id">
+        {/* <Route path="/project/:id">
           render={(props) => (<ProjectPage {...props}/>)}
-        </Route>
+        </Route> */}
       </Switch>
     </ChakraProvider>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,64 @@
 import React, { useEffect, useState } from 'react';
 import ProjectCard from './ProjectCard';
-import { Grid, GridItem } from '@chakra-ui/react';
+import NewProjectForm from '../Forms/NewProjectForm';
+import {
+  Grid,
+  GridItem,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useDisclosure,
+} from '@chakra-ui/react';
 
 const Home = props => {
-  const { currentUser, projects } = props;
-  const fullName = currentUser.first_name + ' ' + currentUser.last_name;
+  const { currentUser, projects, setProjects } = props;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [projectTitle, setProjectTitle] = useState('');
+
+  const submitNewProject = () => {
+    fetch('http://localhost:3000/projects', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        title: projectTitle,
+        user_id: currentUser.id,
+      }),
+    })
+      .then(resp => resp.json())
+      .then(project => {
+        onClose();
+        setProjects([...projects, project])
+      });
+  };
+
   return (
     <div>
-      <h1>Welcome {fullName}</h1>
-
       <Grid
         templateColumns="repeat(2, 1fr)"
         gap={6}
         w="50%"
-        style={{ margin: '0 auto'}}
+        style={{ margin: '0 auto' }}
       >
         {projects.map(project => (
           <GridItem>
             <ProjectCard key={project.id} project={project} />
           </GridItem>
         ))}
+        <Button onClick={onOpen}>Make New Project</Button>
+        <NewProjectForm
+          projectTitle={projectTitle}
+          setProjectTitle={setProjectTitle}
+          isOpen={isOpen}
+          onClose={onClose}
+          submitNewProject={submitNewProject}
+        />
       </Grid>
     </div>
   );
